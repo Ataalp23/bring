a1ae8c1622edb4ab54694f21977f673f6323649e
Ataalp23 <146966018+Ataalp23@users.noreply.github.com>
2025-12-12 21:25:51 +0300

    Update API_URL retrieval to remove default value
    Removed default API URL from secrets retrieval.

--- appp.py
+++ appp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = st.secrets.get("API_URL", "http://127.0.0.1:8000")
+API_URL = st.secrets.get("API_URL", ")
 
 st.title("TalepBazlı Emlak (Ters Sahibinden)")
 
